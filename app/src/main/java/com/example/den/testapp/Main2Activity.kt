deda1be548c7e923225063fbad7f2a317bac67c6
Denis Rasulev <ranklord@gmail.com>
2017-10-29 18:49:07 +0100

    init

---
+++ app/src/main/java/com/example/den/testapp/Main2Activity.kt
@@ -0,0 +1,40 @@
+package com.example.den.testapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main2.*
+import java.util.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        showRandomNumber()
+    }
+
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
+
+    private fun showRandomNumber() {
+        // Get the count from the intent extras
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+
+        // Generate the random number
+        val random = Random()
+        var randomInt = 0
+        // Add one because the bound is exclusive
+        if (count > 0) {
+            // Add one because the bound is exclusive
+            randomInt = random.nextInt(count + 1)
+        }
+
+        // Display the random number
+        textView_random.text = randomInt.toString()
+
+        // Substitute the max value into the string resource for the heading, and update the heading
+        textView_second.text = getString(R.string.second_screen_text, count)
+    }
+
+}
